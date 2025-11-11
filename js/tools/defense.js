2bb0d4e8ade5925ef88bcd6aaa380405624033bb
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2025-11-11 15:47:46 +0100

    Defense examples

--- js/tools/defense.js
+++ js/tools/defense.js
@@ -29,41 +29,133 @@ class DefenseCalculator extends ToolBase
 		add_to(this.tree[0], "br", {br:true});
 		this.result = add_to(
 			this.tree[0],
-			"span",
-			{
-				innerhtml:"<b>Example:</b><br>Base Damage: 100000<br>Final Damage: 100000"
-			}
+			"span"
 		);
+		this.calculate();
 	}
 	
 	calculate()
 	{
 		try
 		{
-			let f = parseFloat(this.input.value);
-			if(isNaN(f))
+			let base_f = parseFloat(this.input.value);
+			if(isNaN(base_f))
 			{
 				throw new Error("The input isn't a valid float");
 			}
-			else if(f < -99)
+			else if(base_f < -99)
 			{
 				throw new Error("The input is under the lower limit");
 			}
-			f /= 100; // percent conversion
+			let f = base_f / 100; // percent conversion
 			f = 1 / (1 + f);
 			f = (Math.floor(f * 10000) / 10000);
+			let text = "";
 			if(f > 1)
 			{
-				this.result.innerHTML = "<b>Example:</b><br>Base Damage: 100000<br>Final Damage: " + Math.round(100000 * f) + "<br>Damage are increased: x" + f;
+				text = "<b>Example:</b><br>Base Damage: 100000<br>Final Damage: " + Math.round(100000 * f) + "<br>Damage are increased: x" + f + "<br>";
 			}
 			else if(f < 1)
 			{
-				this.result.innerHTML = "<b>Example:</b><br>Base Damage: 100000<br>Final Damage: " + Math.round(100000 * f) + "<br>Damage are reduced: x" + f;
+				text = "<b>Example:</b><br>Base Damage: 100000<br>Final Damage: " + Math.round(100000 * f) + "<br>Damage are reduced: x" + f + "<br>";
 			}
 			else
 			{
-				this.result.innerHTML = "<b>Example:</b><br>Base Damage: 100000<br>Final Damage: 100000";
+				text = "<b>Example:</b><br>Base Damage: 100000<br>Final Damage: 100000<br>";
 			}
+			let base_def = Math.floor((base_f * 0.1 + 10) * 10) / 10;
+			switch(base_f)
+			{
+				case 0: // 10 def
+				{
+					text += "<br>Content with 10 (unmodified) base defense: Estimated Damage, Trial fights, Story fights, UnF Extreme+";
+					text += "<br>";
+					break;
+				}
+				case -5: // 9.5 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Fediel (Impossible)";
+					text += "<br>";
+					break;
+				}
+				case -15: // 8.5 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Six-Dragon Black Advent";
+					text += "<br>";
+					break;
+				}
+				case 10: // 11 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Proto-Bahamut (normal), Ultimate Bahamut HL";
+					text += "<br>";
+					break;
+				}
+				case 15:
+				{
+					text += "<br>Notable effects with such value: Super Ultimate Bahamut's 1 earth tenet";
+					text += "<br>";
+					break;
+				}
+				case 20: // 12 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Omega HL, Qilin & Huanglong HL, Metatron, Avatar, Grand Order HL, Lindwurm";
+					text += "<br>";
+					break;
+				}
+				case 30: // 13 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Primarchs (N & HL), Ultimate Bahamut N, Shiva, Europa, Alexiel, Grimnir";
+					text += "<br>";
+					break;
+				}
+				case 50: // 15 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Malice, Menace, Akasha, Lucilius, Astaroth, Pride of Ascendant";
+					text += "<br>";
+					break;
+				}
+				case 80: // 18 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Rose Queen, Six-Dragons HL, Super Ultimate Bahamut";
+					text += "<br>";
+					break;
+				}
+				case 100: // 20 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Proto Bahamut HL, Lucilius (Hard), Beelzebub";
+					text += "<br>";
+					break;
+				}
+				case 120: // 22 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Celeste HL (Mist form)";
+					text += "<br>";
+					break;
+				}
+				case 150: // 25 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Beelzebub, UnF NM150-200";
+					text += "<br>Notable effects with such value: Super Ultimate Bahamut's 10 earth tenets";
+					break;
+				}
+				case 200: // 30 def
+				{
+					text += "<br>Content with " + base_def + " base defense: Rose Queen (Dark form)";
+					text += "<br>";
+					break;
+				}
+				case 1000: // guard
+				{
+					text += "<br>";
+					text += "<br>Notable effects with such value: Guarding, Alexiel (Summon)'s defense buff";
+					break;
+				}
+				default:
+				{
+					text += "<br><br>";
+				}
+			}
+			this.result.innerHTML = text;
 		}
 		catch(err)
 		{
