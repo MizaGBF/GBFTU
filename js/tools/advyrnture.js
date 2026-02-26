3f6fc66eeb7899df5437de9eb92a9621b5ac77f5
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-02-26 16:20:11 +0100

    Forgot Vania's bloodkin

--- js/tools/advyrnture.js
+++ js/tools/advyrnture.js
@@ -168,6 +168,7 @@ class AdvyrntureOptimizer extends ToolBase
 		"122":{name:"Fresh Cucumber",combat:0,perception:0,endurance:0,affinity:0,luck:3,skill:[4,"Kyuta"]},
 		"123":{name:"Cat Teaser",combat:0,perception:0,endurance:0,affinity:3,luck:0,skill:[4,"Young Cat"]},
 		"124":{name:"Beach Ball",combat:4,perception:0,endurance:0,affinity:0,luck:4,skill:[5,"Ursula"]},
+		"125":{name:"Vania's Bloodkin",combat:6,perception:4,endurance:0,affinity:0,luck:0,skill:[1,"Medvecia Island","perception",8]},
 		//"126":{name:"?",combat:0,perception:0,endurance:0,affinity:0,luck:0,skill:[5,"Malinda"]},
 		"127":{name:"Codex of Mysteria",combat:5,perception:7,endurance:0,affinity:0,luck:0,skill:[1,"Mysteria Academy of Magic","affinity",8]}
 	});
