a1ac6ec8300f1725b0f5248cc6e0df489df09c4e
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-07-28 10:05:02 +0200

    Taboo (Hard) raids

--- js/tools/youtube_search.js
+++ js/tools/youtube_search.js
@@ -37,6 +37,8 @@ class YoutubeSearch extends ToolBase
 		{jp:"マクタンマカル", value:"Macutanmacar", asset:"sp/quest/assets/lobby/305741.png"},
 		{jp:"パパルクルヴァ", value:"Papahlukruva", asset:"sp/quest/assets/lobby/305751.png"},
 		{jp:"ザマルヴォッチ", value:"Zamalvoch", asset:"sp/quest/assets/lobby/305761.png"},
+		{group:true, label:"Taboo (Hard)"},
+		{jp:"ニフイヴィンテhl", value:"Nihuyvintae (Hard)", asset:"sp/quest/assets/lobby/305781.png"},
 		{group:true, label:"Six Dragons"},
 		{jp:"ウィルナスhl", value:"Wilnas", asset:"sp/quest/assets/lobby/305191.png"},
 		{jp:"ワムデュスhl", value:"Wamdus", asset:"sp/quest/assets/lobby/305201.png"},
